67370bfb746c11a07628340b1bcd3632f1d26f1c
RetricSu <inneverland2013@gmail.com>
2022-10-05 14:43:06 +0800

    fix

--- crates/bm-search/src/lib.rs
+++ crates/bm-search/src/lib.rs
@@ -2,22 +2,23 @@ use std::cmp;
 
 const NO_OF_CHARS: usize = 256;
 
-pub fn bad_char_heuristic(str: String, size: i32) -> [i32; NO_OF_CHARS] {
+pub fn bad_char_heuristic(str: &str, size: i32) -> [i32; NO_OF_CHARS] {
     let init_val: i32 = -1;
     let mut bad_char = [init_val; NO_OF_CHARS];
 
     for i in 0..size {
         let k = str.as_bytes()[i as usize];
-        println!("k: {}, size: {}, i: {}", k, size, i);
         bad_char[k as usize] = i;
     }
     return bad_char;
 }
 
-pub fn search(txt: &str, pattern: &str) {
+pub fn search(txt: &str, pattern: &str) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
     let n = txt.len();
     let m = pattern.len();
-    let bad_char = bad_char_heuristic(pattern.to_string(), m as i32);
+
+    let bad_char = bad_char_heuristic(pattern, m as i32);
 
     let mut s = 0;
 
@@ -32,7 +33,8 @@ pub fn search(txt: &str, pattern: &str) {
         }
 
         if j < 0 {
-            println!("Patterns occur at shift = {}", s);
+            // push occurrence position into result
+            result.push(s as i32);
 
             /* Shift the pattern so that the next
             character in text aligns with the last
@@ -61,10 +63,10 @@ pub fn search(txt: &str, pattern: &str) {
             character. */
 
             let pos = txt.as_bytes()[s + j as usize];
-            println!("j: {}, {}", j, bad_char[pos as usize] as usize);
-            s += cmp::max(1, j as usize - bad_char[pos as usize] as usize);
+            s += cmp::max(1 as i32, j as i32 - bad_char[pos as usize] as i32) as usize;
         };
     }
+    result
 }
 
 #[cfg(test)]
@@ -72,9 +74,18 @@ mod tests {
     use crate::search;
 
     #[test]
-    fn test() {
+    fn t1() {
         let txt = "ABAAABCDABC";
         let pat = "ABC";
-        search(txt, pat);
+        let v = search(txt, pat);
+        assert_eq!(v, [4, 8]);
+    }
+
+    #[test]
+    fn t2() {
+        let txt = "hello, myllo";
+        let pat = "llo";
+        let v = search(txt, pat);
+        assert_eq!(v, [2, 9]);
     }
 }
